1d420e9473149e304af68d40f97771d40e0c1067
ATechnoHazard <amolele@gmail.com>
2019-02-15 13:19:54 +0530

    myFFCS: signUp: Implement basic email auth
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/com/dscvit/android/myffcs/SignInActivity.java
+++ app/src/main/java/com/dscvit/android/myffcs/SignInActivity.java
@@ -1,9 +1,11 @@
 package com.dscvit.android.myffcs;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.dscvit.android.myffcs.fragments.SignInFragment;
 import com.dscvit.android.myffcs.fragments.SignUpFragment;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -15,18 +17,22 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        SegmentedButtonGroup segmentedButtonGroup = findViewById(R.id.segmentedButtonGroup);
-        fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignInFragment()).commit();
-        segmentedButtonGroup.setOnClickedButtonListener(position -> {
-            switch (position) {
-                case 0:
-                    fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignInFragment()).commit();
-                    break;
-                case 1:
-                    fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignUpFragment()).commit();
-                    break;
-            }
-        });
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            SegmentedButtonGroup segmentedButtonGroup = findViewById(R.id.segmentedButtonGroup);
+            fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignInFragment()).commit();
+            segmentedButtonGroup.setOnClickedButtonListener(position -> {
+                switch (position) {
+                    case 0:
+                        fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignInFragment()).commit();
+                        break;
+                    case 1:
+                        fragmentManager.beginTransaction().replace(R.id.sign_in_container, new SignUpFragment()).commit();
+                        break;
+                }
+            });
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
+        }
     }
 }
--- app/src/main/java/com/dscvit/android/myffcs/fragments/SignUpFragment.java
+++ app/src/main/java/com/dscvit/android/myffcs/fragments/SignUpFragment.java
@@ -1,19 +1,35 @@
 package com.dscvit.android.myffcs.fragments;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.dscvit.android.myffcs.MainActivity;
 import com.dscvit.android.myffcs.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment {
+    private static final String TAG = "SignUpFragment";
+    FirebaseAuth firebaseAuth;
 
 
     public SignUpFragment() {
@@ -22,9 +38,60 @@ public class SignUpFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        //updateUI(currentUser);
+
+        // get refs to views
+        TextInputEditText emailEditText = view.findViewById(R.id.signup_email_edittext);
+        TextInputEditText passwordEditText = view.findViewById(R.id.signup_password_edittext);
+        Button registerButton = view.findViewById(R.id.register_button);
+        Button skipButton = view.findViewById(R.id.skip_register_button);
+
+        registerButton.setOnClickListener(l -> {
+            if (Objects.requireNonNull(emailEditText.getText()).toString().isEmpty()) {
+                emailEditText.setError("Email must not be empty!");
+            } else if (Objects.requireNonNull(passwordEditText.getText()).toString().isEmpty()) {
+                passwordEditText.setError("Password must not be empty!");
+            } else if (passwordEditText.getText().toString().length() < 8) {
+                passwordEditText.setError("Password length must be greater than 8 characters!");
+            } else {
+                firebaseAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                        .addOnCompleteListener(requireActivity(), task -> {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "onViewCreated: Sign up successful");
+                                updateUI(firebaseAuth.getCurrentUser());
+                            } else {
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                    Toast.makeText(requireContext(), "This user already exists. Try signing in instead", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(requireContext(), "Registration failed. Try again later", Toast.LENGTH_LONG).show();
+                                    Log.w(TAG, "onViewCreated: Registration failed", task.getException());
+                                }
+                            }
+                        });
+            }
+        });
+        skipButton.setOnClickListener(l -> {
+            Context context = requireContext();
+            context.startActivity(new Intent(context, MainActivity.class));
+        });
+
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            Context context = requireContext();
+            context.startActivity(new Intent(context, MainActivity.class));
+            Log.d(TAG, "updateUI: Starting MainActivity");
+        }
+    }
 }
