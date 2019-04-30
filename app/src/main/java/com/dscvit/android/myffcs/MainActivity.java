5684c57d50f6140e172874580a77b12397faa8f7
ATechnoHazard <amolele@firehound.org>
2019-05-01 00:31:26 +0530

    Show user's profile pic in the main activity * Show a dialog to sign out upon tapping on it * Sign out will also wipe the database
    Signed-off-by: ATechnoHazard <amolele@firehound.org>

--- app/src/main/java/com/dscvit/android/myffcs/MainActivity.java
+++ app/src/main/java/com/dscvit/android/myffcs/MainActivity.java
@@ -1,20 +1,45 @@
 package com.dscvit.android.myffcs;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
+import com.bumptech.glide.Glide;
 import com.dscvit.android.myffcs.fragments.CourseSelectionFragment;
+import com.dscvit.android.myffcs.models.CourseViewModel;
 import com.dscvit.android.myffcs.utils.CourseRepository;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        CourseViewModel viewModel = ViewModelProviders.of(this).get(CourseViewModel.class);
+        CourseRepository repository = new CourseRepository(getApplication());
         setContentView(R.layout.activity_main);
-        new CourseRepository(getApplication()).updateAllCourses();
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new CourseSelectionFragment()).commit();
+        repository.updateAllCourses();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        CircleImageView accountPic = findViewById(R.id.account_pic);
+        if (currentUser != null) {
 
+            Glide.with(MainActivity.this).load(currentUser.getPhotoUrl()).placeholder(R.drawable.ic_person).fitCenter().into(accountPic);
+            accountPic.setOnClickListener(v -> new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Sign out?")
+                    .setMessage("You will lose your saved timetable. However, you can resync it from the cloud if you have saved it already.")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        FirebaseAuth.getInstance().signOut();
+                        viewModel.deleteAllCourses();
+                        startActivity(new Intent(MainActivity.this, SignInActivity.class));
+
+                    }).setNegativeButton("No", ((dialog, which) -> dialog.dismiss())).show());
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new CourseSelectionFragment()).commit();
     }
 }
